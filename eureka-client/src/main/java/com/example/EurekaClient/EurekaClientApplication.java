35d0f06e28aada0bf1042ef2344bc44562377c6f
Dhivyamary <dhivy93diya@gmail.com>
2022-10-08 20:31:20 +0530

    eureka client first commit

---
+++ eureka-client/src/main/java/com/example/EurekaClient/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.example.EurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    public String home() {
+        return "Happy eureka client home project!";
+    }
+
+}
